22e2ec8ff5da9d7c6dd0496d06df95d2d9c06bc7
Philip Salter <philipleesalter@gmail.com>
2016-02-07 14:42:17 -0700

    finish first 3 buttons

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function add(){
-  var equation = '1 + 1';
-  equation += ' = ' + eval(equation);
-  console.log(equation);
+function calculate(value){
+  if(value === 1){
+    buildResponse(value)
+  }else if(value===2){
+    buildResponse(value)
+  }else{
+    document.getElementById('result').innerText='';
+    document.getElementById('result');
+    var text = document.createElement('p');
+    text.innerText = 'You clicked the wrong button!!!';
+    result.appendChild(text);
+  }
 }
+
+function buildResponse(value){
+  document.getElementById('result').innerText='';
+  document.getElementById('result');
+  var text = document.createElement('p');
+  text.innerText = 'you pressed button ' + value;
+  result.appendChild(text);
+}
+
+// document.querySelector('.button').addEventListener('click', function (e) {
+  // console.log('you clicked my button', e);
+  // document.getElementById('result');
+  // var text = document.createElement('p');
+  // text.innerText = 'you pressed button ';
+  // result.appendChild(text);
+// });
